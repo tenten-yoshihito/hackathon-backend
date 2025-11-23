1fc23c37c7a12fc6f03bc4c52e536009ab437194
Yoshihito Nakata <tenten.yoshihito@gmail.com>
2025-11-23 19:05:42 +0900

    ageのバリデーションをgoogle認証との互換性のため削除

--- model/user.go
+++ model/user.go
@@ -4,8 +4,6 @@ import "time"
 
 // MaxNameLen definition of user rule
 const MaxNameLen = 50
-const MinAge = 20
-const MaxAge = 80
 
 // User struct and method used for operation related to user
 type User struct {
@@ -19,11 +17,12 @@ type User struct {
 
 // UserCreateRequest struct and method used to create user
 type UserCreateRequest struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
 }
 
 // IsValid バリデーション
 func (req *UserCreateRequest) IsValid() bool {
-	return req.Name != "" && len(req.Name) <= MaxNameLen && req.Age > MinAge && req.Age < MaxAge
+	return req.Name != "" && len(req.Name) <= MaxNameLen
 }
